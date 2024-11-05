41814dc217f84f3acf51f7e91cddb9dab5fabf38
Santillan Bruno <brunosaantillan@gmail.com>
2024-11-05 19:14:12 -0300

    se borro el proyecto

--- my-app/assets/markers.ts
+++
@@ -1,9 +0,0 @@
-export const markers = [
-    {
-        name: 'Kiosco Los Hermanos',
-        latitude: -34.864698,
-        longitude: -58.045889,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01
-    }
-]
